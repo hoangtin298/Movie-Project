6bc97c48df885ba284acb99141595655c383d3fd
hoangtin69 <hoangtin69@gmail.com>
2021-05-27 17:30:18 +0700

    cinemalist, chua responsive

--- src/components/CinemaList/index.js
+++ src/components/CinemaList/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { actGetCinemaByIdApi, actGetCinemaListApi } from "./modules/action";
-import { makeStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
@@ -9,7 +8,9 @@ import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import Avatar from "@material-ui/core/Avatar";
 import { withStyles } from "@material-ui/core/styles";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import { useStyles } from "./style";
 
 const StyledTab = withStyles({
   wrapper: {
@@ -51,92 +52,8 @@ function TabPanelCinema(props) {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-    display: "flex",
-    height: 630,
-    maxHeight: 630,
-    overflow: "hidden",
-  },
-  tabs: {
-    position: "relative",
-    borderRight: `1px solid ${theme.palette.divider}`,
-
-    border: "1px solid #ebebec",
-  },
-  cinemaLogoTab: {
-    padding: 20,
-    minWidth: "unset",
-
-    "&:after": {
-      content: "''",
-      display: "block",
-      position: "absolute",
-      width: "80%",
-      height: 1,
-      bottom: 0,
-      background: "rgba(238,238,238,.88)",
-    },
-  },
-  cinemaLogoAvatar: {
-    width: 50,
-    height: 50,
-  },
-  cinemaTab: {
-    position: "relative",
-    height: 90,
-    textAlign: "left",
-    padding: "20px 15px 15px 20px",
-    width: 280,
-
-    "&:after": {
-      content: "''",
-      display: "block",
-      position: "absolute",
-      width: "80%",
-      height: 1,
-      bottom: 0,
-      background: "rgba(238,238,238,.88)",
-    },
-  },
-  cinemaByIdList: {
-    border: "1px solid #ebebec",
-    borderLeft: "none",
-  },
-  cinemaTenCumRap: {
-    display: "-webkit-box",
-    WebkitLineClamp: "1",
-    WebkitBoxOrient: "vertical",
-    textOverflow: "ellipsis",
-    overflow: "hidden",
-
-    lineHeight: 1.4,
-    color: theme.palette.secondary.light,
-    fontWeight: theme.typography.fontWeightMedium,
-  },
-  cinemaDiaChi: {
-    display: "-webkit-box",
-    WebkitLineClamp: "1",
-    WebkitBoxOrient: "vertical",
-    textOverflow: "ellipsis",
-    overflow: "hidden",
-
-    color: theme.palette.grey[600],
-    fontWeight: theme.typography.fontWeightRegular,
-  },
-  cinemaChiTiet: {
-    textDecoration: "none",
-    textTransform: "lowercase",
-    fontSize: theme.typography.h6.fontSize,
-    color: theme.palette.primary.main,
-  },
-}));
-
 function CinemaList() {
   const dispatch = useDispatch();
-  const history = useHistory();
   const cinemaList = useSelector((state) => state.cinemaListReducer);
   const cinemaById = useSelector((state) => state.cinemaByIdReducer);
 
@@ -144,10 +61,6 @@ function CinemaList() {
     dispatch(actGetCinemaListApi());
     dispatch(actGetCinemaByIdApi("BHDStar"));
   }, []);
-
-  if (cinemaById.data) {
-    console.log(cinemaById.data[0].lstCumRap);
-  }
 
   const classes = useStyles();
   const [valueLogo, setValueLogo] = useState(0);
@@ -230,14 +143,42 @@ function CinemaList() {
   const renderMovieByCinema = (arrMovie) => {
     return arrMovie.map((item, index) => {
       return (
-        <div>
+        <div key={item.maPhim} className={classes.movieSingle}>
           <img
             alt={item.tenPhim}
             src={item.hinhAnh}
-            style={{ width: 50, height: 50 }}
-            key={item.maPhim}
+            className={classes.anhPhim}
           />
-          <Typography>{item.tenPhim}</Typography>
+          <div className={classes.phimInfo}>
+            <Typography variant="h3" className={classes.tenPhim}>
+              {item.tenPhim}
+            </Typography>
+
+            <div className={classes.ngayGioChieuContainer}>
+              {item.lstLichChieuTheoPhim
+                .slice(0, 4)
+                .map((childItem, childIndex) => {
+                  return (
+                    <div
+                      key={childItem.maLichChieu}
+                      className={classes.ngayGioChieuBox}
+                    >
+                      <div className={classes.ngayGioChieuInfo}>
+                        <Typography className={classes.ngayChieu}>
+                          {moment(childItem.ngayChieuGioChieu).format(
+                            "DD-MM-YYYY"
+                          )}
+                        </Typography>
+                        <Typography>&nbsp;~&nbsp;</Typography>
+                        <Typography variant="h3" className={classes.gioChieu}>
+                          {moment(childItem.ngayChieuGioChieu).format("HH:mm")}
+                        </Typography>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
         </div>
       );
     });
@@ -258,9 +199,7 @@ function CinemaList() {
       <TabPanelCinema
         valueCinema={valueCinema}
         index={valueCinema}
-        style={{
-          flexGrow: 1,
-        }}
+        className={classes.cinemaTabPanel}
       >
         {cinemaById.data &&
           renderMovieByCinema(
